1fd985d9bb9087f104e5a2ccf4a9270bd07feb25
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2015-12-25 01:49:27 +0100

    admin dashboard view and scope added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  # SET HOMEPAGE
   root to: 'home#index'
+
+  # SET ADMIN DASHBOARD
+  scope "/admin" do
+    get 'dashboard', to: 'dashboard#index'
+  end
+
 end
