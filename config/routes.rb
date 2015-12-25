2496ea237e204d020faa1364fb9208c7b8ca0def
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2015-12-25 02:03:31 +0100

    salons added to admin area

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :salons
   # SET HOMEPAGE
   root to: 'home#index'
 
   # SET ADMIN DASHBOARD
   scope "/admin" do
+    resources :salons
     get 'dashboard', to: 'dashboard#index'
   end
 
